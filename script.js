e428e2f567b9558c02e33bb69ca81c8962bd4515
Oruma1to <60720664+Oruma1to@users.noreply.github.com>
2021-06-13 00:45:26 -0400

    updated paragraph count to detect line breaks

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const notes = JSON.parse(localStorage.getItem('notes'))
+// const savedText = localStorage.getItem('textArea')
 const textArea = document.querySelector('.editor')
 const characters = document.querySelector('.characters')
 const charactersNoWs = document.querySelector('.characters-nows')
@@ -18,17 +18,17 @@ const characterCountNoWs = string => {
 
 // Counts words split by regexp of all non alpha-numeric characters or whitespaces, on google docs hyphenated words are counted as 1. Escapes empty strings.
 const wordCount = string => {
-  return string.split(/\s|[^a-z0-9-]/i).filter(word => word.length > 0).length;
+  return string.split(/\s|[^a-z0-9-']/i).filter(word => word.length > 0).length;
 }
 
-// Counts sentences split by periods, if the period is at the end of the paragraph it adds a space for some reason. Escapes empty strings.
+// Counts sentences split by periods or exclamation point!, if the period is at the end of the paragraph it adds a space for some reason. Escapes empty strings.
 const sentenceCount = string => {
   return string.split(/[.!]/).filter(sentence => sentence.match(/[a-z0-9]/i)).length;
 }
 
-// Counts sentences split by paragraph, used for dom manipulation. Closing tags indicate the start of a new line, we then can count the paragraphs from this point.
+// Counts sentences split by paragraph. I run a simple split by regex line break, then i filter the array of results to only include paragraphs with lengths greater than 0 in order to ignore empty spaces being counted as a paragraph.
 const paragraphCount = string => {
-  return string.split('</p>').filter(para => para.length > 0).length
+  return string.split(/\n/g).filter(paragraph => paragraph.length > 0)
 }
 
 const createBigram = string => {
@@ -52,18 +52,22 @@ const createBigram = string => {
   return arr;
 }
 
-
-
-
-const updateTextArea = (text = '') => {
+const updateStats = () => {
   textArea.addEventListener('input', (e) => {
     const { value } = e.target;
     characters.innerText = characterCount(value)
     charactersNoWs.innerText = characterCountNoWs(value)
     sentences.innerText = sentenceCount(value)
-    words.innerText= wordCount(value)
-    console.log(value.length)
+    words.innerText = wordCount(value)
+    // paragraphs.innerText= paragraphCount(value)
+    console.log(paragraphCount(value))
   })
 }
 
-updateTextArea()
+
+// let autoSave = setInterval(() => {
+//   localStorage.setItem('textArea', JSON.stringify(textArea.value))
+//   console.log(textArea.value)
+// },2000)
+
+updateStats()
