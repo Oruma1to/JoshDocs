22d4a50c83230c3741eb2be7a09a4e36ea3331c9
Oruma1to <60720664+Oruma1to@users.noreply.github.com>
2021-06-13 00:05:13 -0400

    renamed all body to string to be more semantic, ! now also ends a sentence, filtering now matches letters and numbers ignoring casing

--- script.js
+++ script.js
@@ -6,41 +6,41 @@ const words = document.querySelector('.words')
 const paragraphs = document.querySelector('.paragraphs')
 const sentences = document.querySelector('.sentences')
 
-// Count includes white spaces
-const characterCount = body => {
-  return body.length
+// Count includes white spaces, simple length of entered string.
+const characterCount = string => {
+  return string.length
 }
 
-// Count exludes white spaces, Empty strings are escaped when the array is joined back into a block of text.
-const characterCountNoWs = body => {
-  return body.split(/\s/).join('').length;
+// Count exludes white spaces, Empty strings are escaped when the array is joined back into a block of text and the length is returned.
+const characterCountNoWs = string => {
+  return string.split(/\s/).join('').length;
 }
 
-// Counts words split by spaces, on google docs hyphenated words are counted as 1. Escapes empty strings.
-const wordCount = body => {
-  return body.split(/\s|[^a-z0-9]/i).filter(word => word.length > 0).length;
+// Counts words split by regexp of all non alpha-numeric characters or whitespaces, on google docs hyphenated words are counted as 1. Escapes empty strings.
+const wordCount = string => {
+  return string.split(/\s|[^a-z0-9-]/i).filter(word => word.length > 0).length;
 }
 
 // Counts sentences split by periods, if the period is at the end of the paragraph it adds a space for some reason. Escapes empty strings.
-const sentenceCount = body => {
-  return body.split('.').filter(sentence => sentence.length > 0).length;
+const sentenceCount = string => {
+  return string.split(/[.!]/).filter(sentence => sentence.match(/[a-z0-9]/i)).length;
 }
 
 // Counts sentences split by paragraph, used for dom manipulation. Closing tags indicate the start of a new line, we then can count the paragraphs from this point.
-const paragraphCount = body => {
-  return body.split('</p>').filter(para => para.length > 0).length
+const paragraphCount = string => {
+  return string.split('</p>').filter(para => para.length > 0).length
 }
 
-const createBigram = body => {
-  if (body.split(' ').length === 2) return {bigram: body, count: 1}
+const createBigram = string => {
+  if (string.split(' ').length === 2) return {bigram: string, count: 1}
 
   let duo;
   let bigrams = {}
-  body = body.split(' ')
+  string = string.split(' ')
 
   // For loop to create frequency counter
-  for (let idx = 0; idx < body.length - 1; idx++) {
-      duo = body[idx].toLowerCase() + " " + body[idx + 1].toLowerCase() // ignore casing
+  for (let idx = 0; idx < string.length - 1; idx++) {
+      duo = string[idx].toLowerCase() + " " + string[idx + 1].toLowerCase() // ignore casing
       bigrams[duo] = (bigrams[duo] || 0) + 1 // increase counter
   }
 
