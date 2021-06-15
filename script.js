87324c4e3206f170f59d24e71f3dc02ab5385ae8
Oruma1to <60720664+Oruma1to@users.noreply.github.com>
2021-06-15 01:11:51 -0400

    added highlight detection, dom gets painted properly, highlight message pops up while items are being highlighted, dissapears when other event listeners fire off

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// const savedText = JSON.parse(localStorage.getItem('textArea'))
 const textArea = document.querySelector('.editor');
 const genBigrams = document.getElementById('gen-bigram');
 const modalBg = document.querySelector('.modal-bg');
@@ -7,84 +6,87 @@ const bigramsContainer = document.getElementById('bigrams');
 const singleBigrams = document.querySelectorAll('bigram')
 const sortDropDown = document.getElementById('sort')
 
-
-// if(savedText) textArea.innerText = savedText
-
-// Count includes white spaces, simple length of entered string.
+// Sort Functions
 const characterCount = string => {
   return string.length;
 }
-
-// Count exludes white spaces, Empty strings are escaped when the array is joined back into a block of text and the length is returned.
 const characterCountNoWs = string => {
   return string.split(/\s/).join('').length;
 }
-
-// Counts words split by regexp of all non alpha-numeric characters or whitespaces, on google docs hyphenated words are counted as 1. Escapes empty strings.
 const wordCount = string => {
   return string.split(/\s|[^a-z0-9-']/i).filter(word => word.length > 0).length;
 }
-
-// Counts sentences split by periods or exclamation point!, if the period is at the end of the paragraph it adds a space for some reason. Escapes empty strings.
 const sentenceCount = string => {
   return string.split(/[.!?]|\n/).filter(sentence => sentence.length > 0).length;
 }
-
-// Counts sentences split by paragraph. I run a simple split by regex line break, then i filter the array of results to only include paragraphs with lengths greater than 0 in order to ignore empty spaces being counted as a paragraph.
 const paragraphCount = string => {
   return string.split(/\n/g).filter(paragraph => paragraph.length > 0).length;
 }
-
 const createBigrams = string => {
   string = string.split(/[\s\n]|[^a-z0-9-']/i).filter(str => str.length > 0);
   let duo;
   let bigrams = {};
   let arr = [];
-  // For loop to create frequency counter
   for (let idx = 0; idx < string.length - 1; idx++) {
-    duo = string[idx].toLowerCase() + " " + string[idx + 1].toLowerCase(); // ignore casing
-    bigrams[duo] = (bigrams[duo] || 0) + 1; // increase counter
+    duo = string[idx].toLowerCase() + " " + string[idx + 1].toLowerCase();
+    bigrams[duo] = (bigrams[duo] || 0) + 1;
   }
-
   for (const bigram in bigrams) {
     arr.push({ couple: bigram, frequency: bigrams[bigram] });
   }
   return arr;
 }
 
-
-// const updateLS = () => {
-//   const docText = document.querySelector('textarea')
-//   localStorage.setItem('doc', JSON.stringify(docText))
-// }
-
-// setInterval(() => updateLS(), 2000)
-
-
+// Dom Stat updaters
 const updateStats = () => {
   const characters = document.querySelector('.characters');
   const charactersNoWs = document.querySelector('.characters-nows');
   const words = document.querySelector('.words');
   const paragraphs = document.querySelector('.paragraphs');
   const sentences = document.querySelector('.sentences');
+  const stats = document.querySelector('.stats')
+  let selectedText = '';
 
-  textArea.addEventListener('input', (e) => {
+
+  const listenerFunction = (e) => {
     const { value } = e.target;
     characters.innerText = characterCount(value);
     charactersNoWs.innerText = characterCountNoWs(value);
     sentences.innerText = sentenceCount(value);
     words.innerText = wordCount(value);
     paragraphs.innerText = paragraphCount(value);
-    // savedText.innerText = value
+    if (stats.firstChild.nodeName === "H3") stats.removeChild(stats.firstChild)
+  }
+
+  textArea.addEventListener('mouseup', (e) => listenerFunction(e))
+  textArea.addEventListener('input', (e) => listenerFunction(e))
+  
+  document.addEventListener('mouseup', (e) => {
+    if ((e.clientY > 119 && e.clientY < 600) && document.getSelection().toString().length > 0) {
+      selectedText = document.getSelection().toString()
+      characters.innerText = characterCount(selectedText);
+      charactersNoWs.innerText = characterCountNoWs(selectedText);
+      sentences.innerText = sentenceCount(selectedText);
+      words.innerText = wordCount(selectedText);
+      paragraphs.innerText = paragraphCount(selectedText);
+      let msgNode = document.createElement('h3')
+      let highlightMsg = document.createTextNode('Highlighted');
+      msgNode.appendChild(highlightMsg)
+      if (selectedText.length > 0 && stats.firstChild.nodeName !== 'H3') {
+        stats.insertBefore(msgNode, stats.firstChild)
+        console.log(stats.firstChild.nodeName)
+      }
+    }
   })
 }
 
+// All modal functions
 const createBigram = (node) => {
-  let newP = document.createElement('li');
+  let newListEl = document.createElement('li');
   let bigramName = document.createTextNode(`${node.couple}: ${node.frequency}`);
-  newP.appendChild(bigramName);
-  newP.classList.add('bigram');
-  bigramsContainer.appendChild(newP);
+  newListEl.appendChild(bigramName);
+  newListEl.classList.add('bigram');
+  bigramsContainer.appendChild(newListEl);
 }
 
 genBigrams.addEventListener('click', () => {
@@ -101,28 +103,19 @@ sortDropDown.addEventListener('change', () => {
   const parentNode = bigramsContainer;
   const newChildren = createBigrams(textArea.value);
   if (sortDropDown.value === 'sort-ascending') {
-    while (parentNode.firstChild) parentNode.removeChild(parentNode.firstChild)
-    newChildren.sort((a, b) => a.frequency - b.frequency)
-    newChildren.forEach(node => createBigram(node))
+    while (parentNode.firstChild) parentNode.removeChild(parentNode.firstChild);
+    newChildren.sort((a, b) => a.frequency - b.frequency);
+    newChildren.forEach(node => createBigram(node));
   } else if (sortDropDown.value === 'sort-descending') {
-    while (parentNode.firstChild) parentNode.removeChild(parentNode.firstChild)
-    newChildren.sort((a, b) => b.frequency - a.frequency)
-    newChildren.forEach(node => createBigram(node))
+    while (parentNode.firstChild) parentNode.removeChild(parentNode.firstChild);
+    newChildren.sort((a, b) => b.frequency - a.frequency);
+    newChildren.forEach(node => createBigram(node));
   }
 })
 
-  modalClose.addEventListener('click', () => {
-    modalBg.classList.remove('bg-active');
-    bigramsContainer.innerHTML = '';
-  })
-
-
-
-//TODO - save to local storage
-
-// let autoSave = setInterval(() => {
-//   localStorage.setItem('textArea', JSON.stringify(textArea.value))
-//   console.log(textArea.value)
-// },2000)
+modalClose.addEventListener('click', () => {
+  modalBg.classList.remove('bg-active');
+  bigramsContainer.innerHTML = '';
+})
 
 updateStats();
